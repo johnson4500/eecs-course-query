6484f4bb4b878c1accb560ca27faaf99b99189e9
johnson4500 <johnsonhan6500@gmail.com>
2023-12-02 00:40:20 -0500

    Setup integration testing

--- restAPI/src/test/java/com/example/john4500/restAPI/RestApiApplicationTests.java
+++ restAPI/src/test/java/com/example/john4500/restAPI/RestApiApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.john4500.restAPI;
 
+import com.example.john4500.restAPI.models.Course;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class RestApiApplicationTests {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestApiApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate template;
 
 	@Test
-	void contextLoads() {
+	public void shouldReturn200Response() {
+		final Course course = new Course("testname", "testsubject", 30300, "testes", 30303, "tets");
+		final ResponseEntity<String> response = template.postForEntity(String.format("http://localhost:%d/save", port), course, String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
-
 }
