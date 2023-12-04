5483a6559b757dbcced5bd1a4a6c5b5dbf3f787b
johnson4500 <johnsonhan6500@gmail.com>
2023-12-04 00:10:38 -0500

    Update tests

--- restAPI/src/test/java/com/example/john4500/restAPI/RestApiApplicationTests.java
+++ restAPI/src/test/java/com/example/john4500/restAPI/RestApiApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -21,9 +23,13 @@ public class RestApiApplicationTests {
 	private TestRestTemplate template;
 
 	@Test
-	public void shouldReturn200Response() {
-		final Course course = new Course("testname", "testsubject", 30300, "testes", 30303, "tets");
+	public void AddThenDeleteCourseTest() {
+		int course_code = 30300;
+		final Course course = new Course("testname", "testsubject", course_code, "testdesc", 30303, "testprof");
 		final ResponseEntity<String> response = template.postForEntity(String.format("http://localhost:%d/save", port), course, String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		final ResponseEntity<String> deleteResponse = template.exchange(String.format("http://localhost:%d/delete/course/" + course_code, port), HttpMethod.DELETE, HttpEntity.EMPTY, String.class);
+		assertThat(deleteResponse.getBody()).isEqualTo("Deleted course with code: " + course_code);
 	}
 }
